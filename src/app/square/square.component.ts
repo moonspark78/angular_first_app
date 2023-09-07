32225ea4d3aaab0c56de307a936af02131cdf58e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-09-07 18:33:15 +0200

    :rocket: first square component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      square works!
-    </p>
+    <button> {{value}} </button>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class SquareComponent {
+    
+    @Input() value?: 'X' | 'O';
 
 }
